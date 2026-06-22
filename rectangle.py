b6f068e80735a274185434855e632867a04054fd
JonD <sethdotse05@gmail.com>
2026-06-21 23:12:01 -0400

    Defined two functions to calculate area and perimeter of a rectangle

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+# Creating a class to calculate area and perimeter of a  rectangle
+
+# Calculate area of a rectangle
+def calc_area(width, height):
+    area = width * height
+
+    return area
+
+# Calculate perimeter of a rectangle
+def calc_perimeter(width, height):
+    perimeter = (width + height) * 2
+
+    return perimeter
