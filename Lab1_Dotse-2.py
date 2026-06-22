46a850c913d6e88cd3d4523cf84a6fd4c0dd766b
JonD <sethdotse05@gmail.com>
2026-06-21 23:38:16 -0400

    Added while loop

--- Lab1_Dotse-2.py
+++ Lab1_Dotse-2.py
@@ -7,3 +7,29 @@ import rectangle as r
 
 # Both modules have a function named calc_area
 # the aliases are used to avoid a name collision
+
+while True:
+    print("Geometry Calculator")
+    print("-------------------")
+    print("1. Calculate Circle Area")
+    print("2. Calculate Circle Circumference")
+    print("3. Calculate Rectangle Area")
+    print("4. Calculate Rectangle Perimeter")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    elif choice == "5":
+        pass  # exit goes here
+    else:
+        pass 
+   
+
