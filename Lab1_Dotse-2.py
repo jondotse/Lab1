7c644901484db70eb5158c48992a1348178c27bc
JonD <sethdotse05@gmail.com>
2026-06-21 23:19:51 -0400

    Added program purpose and imported classes to run the program

---
+++ Lab1_Dotse-2.py
@@ -0,0 +1,9 @@
+# Create a menu-driven program that calculates the area and perimeter/circumference 
+# of circles and rectangles by importing functions from separate modules.
+# Jonathan Dotse, 6/21/2026
+
+import circle as c
+import rectangle as r
+
+# Both modules have a function named calc_area
+# the aliases are used to avoid a name collision
